1677e24a2c5c89fee902307d387fa9c3006e1d2b
JoKhong <JoKhong@users.noreply.github.com>
2025-11-22 10:15:56 +0800

    Basic programmer

---
+++ CitiesManager.Web/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CitiesManager.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [Route("[action]")]
+        [HttpGet]
+        public string MethodGet()
+        {
+            return "Hello World Get";
+        }
+
+        [Route("[action]")]
+        [HttpPost]
+        public string MethodPost()
+        {
+            return "Hello World Post";
+        }
+
+    }
+}
